723334160a0ea536bff9a9c6669fbdca0f6a3561
Stanley <angstanley68@gmail.com>
2020-09-18 12:54:15 +0700

    refactor(vue): Rearrange imports

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import { firestorePlugin } from 'vuefire'
 
+// setup font faces
+import "typeface-inter"
+import "typeface-quicksand"
+
 // setup tailwind css
 import './assets/css/tailwind.css'
 import './assets/css/main.css'
-
-// setup font faces
-require("typeface-inter");
-require("typeface-quicksand");
 
 // setup plugins
 Vue.use(firestorePlugin)
