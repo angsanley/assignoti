4cb2c4cf7dd9b9028c3a5da9b62905acb96210eb
Stanley <angstanley68@gmail.com>
2020-09-20 00:32:42 +0700

    feat(vue): New channel insert into db

--- src/components/AddChannelModal.vue
+++ src/components/AddChannelModal.vue
@@ -1,10 +1,11 @@
 <template>
     <modal name="add-channel-modal" :min-width="350" :min-height="400" height="auto" :width="350">
-        <form class="flex flex-col space-y-6 m-4">
+        <form class="flex flex-col space-y-6 m-4" @submit.prevent="submitForm()">
             <h5 class="text-primary">Add new channel</h5>
             <div class="flex flex-col space-y-1">
                 <label for="channel" class="text-left">Channel name:</label>
-                <Input type="text" id="channel" name="channel" :required="true" placeholder="Egg Fried Rice"/>
+                <Input v-model="form.channelName" type="text" id="channel" name="channel" :required="true" placeholder="Egg Fried Rice"/>
+                <span v-if="errorMessage" class="text-left text-red-500">{{ errorMessage }}</span>
             </div>
             <Button primary>
                 <div class="text-center w-full">
@@ -18,9 +19,36 @@
 <script>
     import Button from "./Button";
     import Input from "./Input";
+    import * as stringHash from "string-hash";
     export default {
         name: "AddChannelModal",
-        components: {Input, Button}
+        components: {Input, Button},
+        data() {
+            return {
+                form: {
+                    id: 0,
+                    channelName: '',
+                    owners: [this.$firebase.auth().currentUser.uid]
+                },
+                errorMessage: ''
+            }
+        },
+        methods: {
+            submitForm() {
+                const db = this.$firebase.database()
+                const dbRef = db.ref('channels')
+                this.form.id = stringHash(dbRef.push().getKey())
+                const payload = JSON.parse(JSON.stringify(this.form))
+
+                dbRef.push(payload).then(() => {
+                    this.$emit('close')
+                }).catch(e => {
+                    this.errorMessage = e.message
+                })
+            }
+        },
+        mounted() {
+        }
     }
 </script>
 
