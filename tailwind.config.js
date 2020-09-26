5deb9cbb4d004fd2a0296f8de28f72dc960fecd2
Stanley <angstanley68@gmail.com>
2020-09-27 03:40:54 +0700

    feat(tailwind): Add container

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,14 @@ module.exports = {
       'display': ['Quicksand', 'sans-serif'],
       'body': ['Inter', 'sans-serif']
     },
+    container: {
+      padding: {
+        default: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+      },
+    },
   },
   variants: {},
   plugins: [],
