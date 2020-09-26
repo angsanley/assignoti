eb56a6590669cb99a6a4c70ed73193e19606d170
Stanley <angstanley68@gmail.com>
2020-09-27 04:51:36 +0700

    feat(sidebar): Add sidebar items

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -1,13 +1,42 @@
 <template>
-    <div>asd</div>
+    <div class="text-white">
+        <div class="text-center my-4">
+            <h1>Assignoti</h1>
+        </div>
+        <div class="nav-items">
+            <router-link to="/dashboard" class="item" active-class="active" exact><HomeIcon/><span>Home</span></router-link>
+            <router-link to="/dashboard/subscriptions" class="item" active-class="active"><BellIcon/><span>Subscriptions</span></router-link>
+            <router-link to="/dashboard/channels" class="item" active-class="active"><SearchIcon/><span>Explore Channels</span></router-link>
+        </div>
+    </div>
 </template>
 
 <script>
+    import { HomeIcon, BellIcon, SearchIcon } from "vue-feather-icons"
     export default {
-        name: "TheSidebar"
+        name: "TheSidebar",
+        components: {HomeIcon, BellIcon, SearchIcon}
     }
 </script>
 
 <style scoped>
+    .nav-items {
+        @apply flex flex-col text-lg
+    }
 
+    .item {
+        @apply flex text-white no-underline h-16 items-center px-4
+    }
+
+    .item > span {
+        @apply ml-4
+    }
+
+    .item:hover {
+        @apply bg-primary-500
+    }
+
+    .active {
+        @apply font-bold bg-primary-700
+    }
 </style>
