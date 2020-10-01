310835ebc104be6a36f5c49c6b2f78158e81c2b2
Christopher Teddy <vitedocorp@yahoo.com>
2020-10-02 00:42:00 +0700

    feat(vue) : Adding Logout Feature
    implemented logout menu, when user click the photo logo at the right top corner

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="navbar">
+    <div class="navbar" v-click-outside="closeUserCard">
         <div class="inner space-x-2">
             <div class="mobile-items space-x-2">
                 <button @click="openSidebar()" class="toggle-sidebar"><MenuIcon/></button>
@@ -7,8 +7,19 @@
             <div>
                 <h4>Assignoti</h4>
             </div>
-            <button v-if="this.userObj"><img class="user-profile" :src="userObj.photoURL" alt="profile"/></button>
+            <button v-if="this.userObj" @click.prevent="toggleUserCard"><img class="user-profile" :src="userObj.photoURL" alt="profile"/></button>
+            <!-- <a href="#" @click="toggleUserCard()" ><img class="account-pic h-10 w-10" :src="userObj.photoURL" alt="profile picture"/></a> -->
         </div>
+
+        <transition name="fade">
+            <div class="user-card" v-if="showUserCard">
+                <div class="flex flex-col items-center">
+                    <img class="account-pic h-24 w-24" :src="userObj.photoURL" alt="profile picture"/>
+                    <div class="text-sm mt-2 dark-mode:text-gray-400">{{userObj.displayName }}</div>
+                    <a href="#" @click.prevent="logout" class="border-2 rounded-full w-full py-1 mt-4 text-center text-gray-700 dark-mode:text-gray-400 no-underline hover:bg-gray-100 dark-mode:hover:bg-gray-800 focus:outline-none">Logout</a>
+                </div>
+            </div>
+        </transition>
     </div>
 </template>
 
@@ -16,13 +27,34 @@
     import { MenuIcon } from "vue-feather-icons"
     export default {
         name: "TheNavbar",
+        data() {
+            return {
+                showUserCard: false,
+                
+            }
+        },
         components: { MenuIcon },
         methods: {
             openSidebar() {
                 setTimeout( () => {
                     this.$store.dispatch('showSidebar')
                 }, 10)
-            }
+            },
+            toggleUserCard() {
+                this.showUserCard = !this.showUserCard
+            },
+            closeUserCard() {
+                this.showUserCard = false;
+            },
+            logout() {
+                this.$store.dispatch('clearAll');
+                this.$router.push('/login');
+            },
+            // async getPhoto() {
+            //     const GeneralRepository = RepositoryFactory.get('general');
+            //     const response = await GeneralRepository.getPhoto();
+            //     this.profilePicture = `data:image/png;base64,${response.data['photo']}`;
+            // }
         },
         computed: {
             userObj() {
@@ -31,7 +63,7 @@
         },
         mounted() {
             this.$store.dispatch('getAuthenticatedUser')
-        }
+        },
     }
 </script>
 
@@ -62,6 +94,17 @@
 
     .user-profile {
         @apply w-10 h-10 rounded-full
+    }
+
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .2s;
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+        opacity: 0;
+    }
+
+    .user-card {
+        @apply bg-white rounded-lg shadow-xl fixed px-4 py-6 mt-16 mr-6 top-0 right-0;
     }
 
     @screen lg {
